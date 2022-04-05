d1a342cd76751928ca0466a366383202767f9164
j0rd1s3rr4n0 <jordiserrano@protonmail.ch>
2022-04-05 16:06:37 +0200

    v0.8 Views

---
+++ views.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>DKIA | Inicio</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="">
+    </head>
+    <body>
+        <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="/about/">About Us</a></li>
+              <li><a href="/contact/">Contact Us</a></li>
+              <li><a href="/contribute-at-scanfcode/">Contribute</a></li>
+              <li><a href="/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="/sitemap/">Sitemap</a></li>
+            </ul>
+          </div>
+        <nav>
+            <ul>
+                <li><a href="followers.php">Followers</a></li>
+                <li><a href="likes.php">Likes</a></li>
+                <li><a>Views</a></li>
+                <li><a>Comments</a></li>
+            </ul>
+        </nav>
+    </body>
+</html>
