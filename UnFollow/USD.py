362bc82ef68ab81a3701eeda98f5d77d697a517c
j0rd1s3rr4n0 <jordiserrano@protonmail.ch>
2022-04-05 16:07:12 +0200

    v1.0 UnFollow

---
+++ UnFollow/USD.py
@@ -0,0 +1,43 @@
+"""
+
+
+
+
+HI, There This is a simple message ;)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+password = ["Password"
+			]
+username = ["username"
+			]
+
+"""
+usuaris = []
+counting = 0
+while counting != 11:
+	usuarius = 'contrasena'+str(counting)
+	counting +=1
+	usuaris = usuarius
+	print(usuaris)
+"""
---
+++ UnFollow/ddd.cmd
@@ -0,0 +1,2 @@
+python3 -m pyfollow.py -u thedemonsdemon  -f 1 private
+exit 
