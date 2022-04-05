75fe05520ba5b888db9efeb04b8bf1639213573d
j0rd1s3rr4n0 <jordiserrano@protonmail.ch>
2022-04-05 15:58:35 +0200

    v1.1

---
+++ SendFollowers/USD.py
@@ -0,0 +1,43 @@
+"""
+
+
+
+
+HI, There This is a simple message ;)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+password = ["password1","password2"
+			]
+username = ["user","user2"
+			]
+
+"""
+usuaris = []
+counting = 0
+while counting != 11:
+	usuarius = 'contrasena'+str(counting)
+	counting +=1
+	usuaris = usuarius
+	print(usuaris)
+"""
